f8fc63c3001aaf887936a6c8b8152242146d6766
TAbrahamyan <t.abrahamyann@gmail.com>
2021-01-05 22:18:03 +0400

    Implementing login/register design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './client/components/login/login.component';
+import { RegisterComponent } from './client/components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule { }
